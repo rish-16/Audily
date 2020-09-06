7b188a59746fef0fbf971963189a676a278e27ad
rish-16 <mail.rishabh.anand@gmail.com>
2020-09-06 12:43:48 +0800

    Add Flask wrapper to OCR/TTS

--- src/main.py
+++ src/main.py
@@ -1,13 +1,31 @@
-import easyocr
-import gtts
+from flask import Flask, Response
+from flask import send_file
+
+import gtts, random, pytesseract
 from playsound import playsound
+from PIL import Image
 
-reader = easyocr.Reader(['en'])
-result = reader.readtext("../assets/book1.jpg", detail=0)
+app = Flask(__name__)
 
-full_text = " ".join(result)
-print (full_text)
-
-tts = gtts.gTTS(full_text)
-tts.save("recording.mp3")
-playsound("recording.mp3")
+@app.route('/')
+def hello():
+    return "Welcome to the Audily API!"
+    
+@app.route('audify')    
+def audify():
+    img = flask.request.files.get('imagefile', '')
+    full_text = pytesseract.image_to_string(img)
+    print (full_text)
+    
+    random_id = "".join([str(random.randint(0, 9)) for i in range(8)])
+    tts = gtts.gTTS(full_text)
+    
+    path_to_file = "./all_recordings/recording_{}.mp3".format(random_id)
+    tts.save(path_to_file)
+    
+    return send_file(
+            path_to_file, 
+            mimetype="audio/mp3", 
+            as_attachment=True, 
+            attachment_filename="recording_{}.mp3".format(random_id)
+        )
