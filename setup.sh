4b56858fdb58ce1e5a9cb5c15c8da36509033fd6
Aadit Kamat <aadit.k12@gmail.com>
2020-09-12 13:06:35 +0000

    Add virtual environment for python installation

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
-pip install -r requirements.txt
 # Install tesseract via brew
 if [[ -z $(which tesseract) ]]; then
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     brew install tesseract
 fi
+
+# Install python modules
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
