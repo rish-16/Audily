c8981478d846a3e108cd175df3408a4f599b1f78
Aadit Kamat <aadit.k12@gmail.com>
2020-09-12 13:05:36 +0000

    Add brew installation to setup script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 pip install -r requirements.txt
-brew install tesseract
+# Install tesseract via brew
+if [[ -z $(which tesseract) ]]; then
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    brew install tesseract
+fi
